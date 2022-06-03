78e766d7f4a775694616b5b6e99d364a5861b4e0
Lorraine <68146638+rainecodes@users.noreply.github.com>
2022-06-03 17:57:02 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -62,7 +62,6 @@ function showForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "4b3bdecf18d346d5f33fae1e81f02461";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
